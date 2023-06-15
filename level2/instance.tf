13ac53d132a080da5f2bae02b146500ea8efdd7b
Vladimir <awsvlad@gmail.com>
2023-06-15 07:44:44 -0400

    Add load balancer

--- level2/instance.tf
+++ level2/instance.tf
@@ -27,14 +27,6 @@ resource "aws_security_group" "public" {
     cidr_blocks = ["68.228.97.89/32"]
   }
 
-  ingress {
-    description = "HTTP from home office"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["68.228.97.89/32"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -54,7 +46,6 @@ resource "aws_instance" "public" {
   vpc_security_group_ids      = [aws_security_group.public.id]
   key_name                    = "main"
   associate_public_ip_address = true
-  user_data                   = file("user-data.sh")
 
   tags = {
     Name = "${var.env_code}-public"
@@ -74,6 +65,14 @@ resource "aws_security_group" "private" {
     cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
   }
 
+  ingress {
+    description     = "HTTP from load-balancer"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.load-balancer.id]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -87,17 +86,16 @@ resource "aws_security_group" "private" {
 }
 
 resource "aws_instance" "private" {
+  count = length(data.terraform_remote_state.level1.outputs.public_subnet_id)
+
   ami                    = data.aws_ami.amazonlinux.id
   instance_type          = "t3.micro"
-  subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[0]
+  subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[count.index]
   vpc_security_group_ids = [aws_security_group.private.id]
   key_name               = "main"
+  user_data              = file("user-data.sh")
 
   tags = {
     Name = "${var.env_code}-private"
   }
-}
-
-output "public_ip_address" {
-  value = aws_instance.public.public_ip
 }
